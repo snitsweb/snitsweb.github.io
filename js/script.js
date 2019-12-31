da19929f25e970134925078589a86c8ec291236e
snitsweb <59214158+snitsweb@users.noreply.github.com>
2019-12-31 04:29:17 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.main-screen--button-href').click(function(){
+       $('html, body').animate({scrollTop:$('#about-me').position().top}, 1200);
+    });
+
+    $('.link_1').click(function(){
+       $('html, body').animate({scrollTop:$('#about-me').position().top}, 1200);
+    });
+
+    $('.link_2').click(function(){
+       $('html, body').animate({scrollTop:$('#skills').position().top}, 1200);
+    });
+
+    $('.link_3').click(function(){
+       $('html, body').animate({scrollTop:$('#portfolio').position().top}, 1200);
+    });
+});
