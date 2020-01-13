39a882eedb1bd8dc490fbfc537b2bbe2d2eed6ac
snitsweb <59214158+snitsweb@users.noreply.github.com>
2020-01-13 23:00:53 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,4 +14,18 @@ $(document).ready(function() {
     $('.link_3').click(function(){
        $('html, body').animate({scrollTop:$('#portfolio').position().top}, 1200);
     });
+
+
+
+    $(document).bind( 'mousewheel', function (e) { 
+    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
+    e.preventDefault(); 
+    e.stopPropagation(); 
+    $(document.body).stop().animate( { 
+         scrollTop : nt 
+     } , 500 , 'easeInOutCubic' );  
+	})
+
 });
+
+
--- sites/yoga/js/script.js
+++ sites/yoga/js/script.js
@@ -1,12 +1,17 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+    $('.nav-link-1').click(function(){
+       $('html, body').animate({scrollTop:$('#header').position().top}, 2000);
+    });
 
+    $('.nav-link-2').click(function(){
+       $('html, body').animate({scrollTop:$('#why').position().top}, 2000);
+    });
 
-    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
-    var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
-        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-        $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
-        }
-        return false; // выключаем стандартное действие
+    $('.nav-link-3').click(function(){
+       $('html, body').animate({scrollTop:$('#footer__down').position().top}, 2000);
+    });
+
+    $('.nav-link-4').click(function(){
+       $('html, body').animate({scrollTop:$('#footer').position().top}, 2000);
     });
 });
