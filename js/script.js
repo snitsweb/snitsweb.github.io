df4be543df697727a0dc2a04df35345ae1593fc3
snitsweb <59214158+snitsweb@users.noreply.github.com>
2020-01-31 02:33:07 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,17 +15,6 @@ $(document).ready(function() {
        $('html, body').animate({scrollTop:$('#portfolio').position().top}, 1200);
     });
 
-
-
-    $(document).bind( 'mousewheel', function (e) { 
-    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
-    e.preventDefault(); 
-    e.stopPropagation(); 
-    $(document.body).stop().animate( { 
-         scrollTop : nt 
-     } , 500 , 'easeInOutCubic' );  
-	})
-
 });
 
 
